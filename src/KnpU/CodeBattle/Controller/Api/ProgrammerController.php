8d3188166610d479db3055d7fad9e1c6f1615820
binyoma <innocentbinyoma@pm.me>
2025-01-08 23:18:53 +0100

    rest:ressources & representations, POST & GET methods

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -3,6 +3,7 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use KnpU\CodeBattle\Model\Programmer;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,69 @@ class ProgrammerController extends BaseController
 {
     protected function addRoutes(ControllerCollection $controllers)
     {
-        // $controllers->post('/api/programmers', array($this, 'newAction'));
+         $controllers->post('/api/programmers', array($this, 'newAction'));
+         $controllers->get('/api/programmers', array($this, 'listAction'));
+        $controllers->get('/api/programmers/{nickname}', array($this, 'showAction'))
+            ->bind('api_programmers_show');
     }
 
+
+
+    public function newAction(Request $request){
+        $data = json_decode($request->getContent(), true);
+        $programmer = new Programmer();
+        $programmer->nickname = $data['nickname'];
+        $programmer->avatarNumber = $data['avatarNumber'];
+        $programmer->tagLine = $data['tagLine'];
+        $programmer->userId =$this->findUserByUsername('weaverryan')->id;
+
+        $this->save($programmer);
+
+        $url = $this->generateUrl('api_programmers_show', array(
+            'nickname' => $programmer->nickname
+        ));
+
+        $response= new Response('It worked',201);
+        $response->headers->set('Location', $url);
+        return $response;
+
+
+    }
+    public function showAction($nickname)
+    {
+        $programmer = $this->getProgrammerRepository()
+            ->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            $this->throw404('oh no! This programmer does not exist');
+        }
+        $data = $this->serializeProgrammer($programmer);
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    public function listAction()
+    {
+        $programmers = $this->getProgrammerRepository()
+            ->findAll();
+
+        $data = array('programmers' => array());
+
+        foreach ($programmers as $programmer) {
+            $data['programmers'][] = $this->serializeProgrammer($programmer);
+        }
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    private function serializeProgrammer(Programmer $programmer)
+    {
+        return array(
+            'nickname'=> $programmer->nickname,
+            'avatarNumber' => $programmer->avatarNumber,
+            'powerLevel' => $programmer->powerLevel,
+            'tagLine' => $programmer->tagLine,
+        );
+    }
 }
